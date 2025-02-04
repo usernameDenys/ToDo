e2345208c950feeee074e44c14eead2cfe9273a7
Honeybloom0 <amiri.sarah@outlook.com>
2025-02-04 15:40:42 +0100

    ajout de fichier

---
+++ readTask.php
@@ -0,0 +1,47 @@
+<?php
+include_once("./db_con.php");
+?>
+<?php if ($conn) : ?>
+    <?php
+
+
+    $requete = "SELECT * FROM tasks";
+
+
+    $exec = $conn->query($requete);
+    $result = $exec->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <?php
+    foreach ($result as $index => $tasks) : ?>
+
+
+
+        <div class="info-group">
+            <p class="name"><?php echo $tasks['taskName']; ?></p>
+            <p class="lastname"><?php echo $tasks['taskDesc']; ?></p>
+        </div>
+
+        <div class="btn-group">
+            <form class="delete" action="./delete.php" method="POST">
+                <input type="hidden" name="email" value="<?php echo $tasks["id"] ?>">
+                <input type="submit" value="Delete your profile">
+            </form>
+
+            <form class="change" action="./mod-page.php" method="POST">
+                <input type="hidden" name="email" value="<?php echo $tasks["id"] ?>">
+                <input type="submit" value="Change your details">
+            </form>
+        </div>
+
+
+        </div>
+
+
+    <?php endforeach; ?>
+
+
+
+<?php else: ?>
+    <p>Error connection</p>
+<?php endif; ?>
