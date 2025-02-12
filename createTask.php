1841ec303e29e245f07403f3e75b92c64841b1aa
Honeybloom0 <amiri.sarah@outlook.com>
2025-02-12 17:03:30 +0100

    injection sql

--- createTask.php
+++ createTask.php
@@ -1,22 +1,48 @@
-<?php include_once ("./db_con.php")
-?>
-
 <?php 
-$nameTask = htmlspecialchars($_POST ["taskName"]);
+include_once("./db_con.php");
+
+$nameTask = htmlspecialchars($_POST["taskName"]);
 $descTack = htmlspecialchars($_POST["taskDesc"]);
+
+$pattern = "/^[A-Z]{1}[a-z]*$/";
+
+$nameValide = false;
+$descValide = false;
+
+if ($conn):
+
+    if (isset($nameTask) && !empty(trim($nameTask)) && preg_match($pattern, $nameTask) && strlen($nameTask) > 2 && strlen($nameTask) <= 255):
+        $nameValide = true;
+    else:
+        echo "<p>Vérifiez le format du texte.</p>";
+    endif;
+
+    if (isset($descTack) && !empty(trim($descTack))):
+        $descValide = true;
+    else:
+        echo "<p>Vérifiez le format du texte.</p>";
+    endif;
+
+    if ($nameValide === true && $descValide === true):
+
+        $requete = "INSERT INTO tasks (name, description, status) 
+                    VALUES (:taskName, :desc, 0)";
+
+        $etat = $conn->prepare($requete);
+
+        $etat->bindParam(':taskName', $nameTask, PDO::PARAM_STR);
+        $etat->bindParam(':desc', $descTack, PDO::PARAM_STR);
+
+        if ($etat->execute()):
+            header("Location: ./readTask.php");
+            exit();
+        else:
+            echo "<p>Erreur lors de l'insertion.</p>";
+        endif;
+
+    endif; // Fin du test des valeurs
+
+else:
+    echo "<p>Erreur de connexion.</p>";
+endif; 
 ?>
-
-<?php if ($conn):?>
-    
-    <?php
-    $requete = "INSERT INTO tasks (name, description, status) 
-                VALUES ('$nameTask','$descTack', false)";
-              
-    $exec = $conn->query($requete);
-    header("Location: ./readTask.php");
-    exit();
-    ?>
-
-<?php else: ?>
-    <p>Error connection</p>
-<?php endif; ?>
